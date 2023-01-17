bb526f536a8c2c40847678a45693d9a49b7d5077
juunini <juuni.ni.i@gmail.com>
2023-01-18 00:51:26 +0900

    Create open_links.sh

---
+++ open_links.sh
@@ -0,0 +1,7 @@
+open "https://www.google.com/chrome"
+open "https://code.visualstudio.com"
+open "https://iterm2.com/downloads/stable/latest"
+open "https://pilotmoon.com/scrollreverser"
+open "https://www.spectacleapp.com"
+open "https://desktop.github.com"
+
